fb592995b39785b6bb0d8afb510570d4dca75468
Faltas <Girgis.Faltas@Honeywell.com>
2025-12-29 10:41:38 +0100

    Aggiunta pagina di login con SSO

--- app/settings/page.jsx
+++ app/settings/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Sidebar } from "@/components/Sidebar";
 import { MobileSideBar } from "@/components/MobileSideBar";
 import { TopBar } from "@/components/TopBar";
